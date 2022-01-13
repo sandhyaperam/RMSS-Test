7c63ec051640116456b65f2ccf94f0b764662d5a
sandhyaperam <82131500+sandhyaperam@users.noreply.github.com>
2022-01-13 20:57:31 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using InventoryApplication.Models;
+namespace NetCoreWebApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.  
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.  
+            services.AddMvc();
+            services.Add(new ServiceDescriptor(typeof(MusicStoreContext), new MusicStoreContext(Configuration.GetConnectionString("DefaultConnection"))));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.  
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+
+                routes.MapRoute(
+               name: "areaRoute",
+               template: "{area:exists}/{controller=AdminDashBoard}/{action=DashBoard}/{id?}"
+           );
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Admin}/{action=Login}/{id?}");
+
+            });
+        }
+    }
+}
